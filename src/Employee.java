270d5628317dc54f5ad7267eef2d2b8180f54ded
Himanshu <himamshuthakur20@gmail.com>
2024-01-22 11:35:25 +0530

    Added UC2 : Calculate Salary

--- src/Employee.java
+++ src/Employee.java
@@ -2,10 +2,14 @@ public class Employee {
     String name;
     Integer id;
 
+    Integer wagePerHour;
+
+    Integer workHour = 0;
     boolean attendence;
 
-    Employee(String n, Integer i){
+    Employee(String n, Integer i,Integer w){
         this.name = n;
         this.id = i;
+        this.wagePerHour = w;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Employee e1 = new Employee("Bharat",1);
+        Employee e1 = new Employee("Bharat",1,200);
         int check = (int)((Math.random()*10))%2;
 
         if (check == 0){
@@ -11,8 +11,20 @@ public class Main {
 
         if(e1.attendence){
             System.out.println("Employee is present");
+            e1.workHour = 8;
         }else {
             System.out.println("Employee is absent");
         }
+
+        //it is a Not static method
+        Main m1 = new Main();
+        System.out.println(m1.calculateSalary(e1));
+
+
+    }
+    double calculateSalary(Employee e){
+        return e.workHour*e.wagePerHour;
     }
 }
+
+
